a33b2ae06fedb94a9cf0f4c1c0c0c2ce68fae847
Fidan Oruc <fidanorucc>
2023-10-18 18:08:23 +0200

    Implement the Complete and Exit buttons

--- gui.py
+++ gui.py
@@ -5,18 +5,16 @@ label = gui.Text("Type in a to-do")
 input_box = gui.InputText(tooltip="Enter to-do", key="todo")
 add_button = gui.Button("Add")
 list_box = gui.Listbox(values=functions.get_todos(),
-                       key="todos", enable_events=True, size=(20, 10))
+                       key="todos", enable_events=True, size=(44, 10))
 edit_button = gui.Button("Edit")
 complete_button = gui.Button("Complete")
+exit_button = gui.Button("Exit")
 
-window = gui.Window("My To-Do_App",
-                    layout=[[label], [input_box, add_button], [list_box, edit_button, complete_button]],
-                    font=('Helvetica', 20))
+layout = [[label], [input_box, add_button], [list_box, edit_button, complete_button], [exit_button]]
+window = gui.Window("My To-Do_App", layout=layout, font=('Helvetica', 20))
 
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -27,15 +25,25 @@ while True:
         case "Edit":
             todo_to_edit = values["todos"][0]
             new_todo = values['todo'] + "\n"
+
             todos = functions.get_todos()
+
             index = todos.index(todo_to_edit)
-            print(index)
+
             todos[index] = new_todo.title()
             functions.write_todos(todos)
-            # listbox instance key="todos"
+            window["todos"].update(values=todos)  # listbox instance key="todos"
+        case "Complete":
+            todo_to_complete = values["todos"][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
             window["todos"].update(values=todos)
+            window["todo"] .update(value="")
         case "todos":
             window["todo"].update(value=values["todos"][0])
+        case "Exit":
+            break
         case gui.WIN_CLOSED:
             break
 
