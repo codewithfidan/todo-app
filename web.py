0aa9349e1a51f2e2a6da8a9139f5c8952723e58f
Fidan Oruc <fidanorucc>
2023-11-05 11:54:55 +0100

    Implement "Delete todo"

--- web.py
+++ web.py
@@ -14,8 +14,15 @@ def add_todo():
 st.title("My To-Do App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
-for todo in todos:
-    st.checkbox(todo)
 
-st.text_input("Enter a todo:", placeholder="Add a new todo...",
-              on_change=add_todo, key="new_todo")
+# Delete a todo
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+
+st.text_input(label="", placeholder="Enter a todo:", on_change=add_todo, key="new_todo")
